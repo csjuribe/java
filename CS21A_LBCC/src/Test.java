132e651a2cc7927e1755f5dd3af558bcb5225ce8
juan.uribe <juan@uribe.ws>
2013-08-30 01:18:24 -0700

    This is the first test commit on my Mac OS X machine using Git and programming with java.
    These files are public for now. The java projects will be private until
    completion of CS21A - Java Programming course @ LBCC in December 2013.

---
+++ CS21A_LBCC/src/Test.java
@@ -0,0 +1,13 @@
+/** Created this file to test using git
+*	Author: Juan A. Uribe
+*	Date: August 31, 2013
+*	Subject: Java Programming & using GIT to 
+*	version control projects
+*/
+
+public class Test {
+	public static void main( String[] args) {
+		System.out.println( "This is a test." ) ;
+	}
+	
+}
